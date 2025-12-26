f47abdb45ed459c1354758af33f9bb508361f583
emad karimi <emad.k50000@gmail.com>
2025-12-26 00:55:42 +0000

    ناوبار هنوز سمت چپه که

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -7,8 +7,8 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-background" dir="rtl">
-        <div className="flex flex-1">
+    <div className="flex min-h-screen w-full bg-background" dir="rtl">
+        <div className="flex flex-1 flex-row-reverse">
             <AdminSidebar />
             <main className="flex-1 p-4 md:p-8">
                 <AdminHeader />
