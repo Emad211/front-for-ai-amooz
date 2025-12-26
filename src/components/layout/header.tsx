2022860ba8a3d2c7eae414caef21cb3dab02bf3a
emad karimi <emad.k50000@gmail.com>
2025-12-26 09:53:44 +0000

    studio-3863900619:~/studio{(no branch, rebasing main)}$ git pull origin

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -55,7 +55,7 @@ const UserProfile = () => (
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-           <DropdownMenuItem asChild>
+          <DropdownMenuItem asChild>
             <Link href="/profile">
               <User className="ml-2 h-4 w-4" />
               <span>پروفایل</span>
